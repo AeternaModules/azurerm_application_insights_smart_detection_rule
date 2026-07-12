fbc3ab6e65a52a7890ec8630bb603353c36cb9dc
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "application_insights_smart_detection_rules" {
-  description = "All application_insights_smart_detection_rule resources"
-  value       = azurerm_application_insights_smart_detection_rule.application_insights_smart_detection_rules
-}
 output "application_insights_smart_detection_rules_additional_email_recipients" {
-  description = "List of additional_email_recipients values across all application_insights_smart_detection_rules"
-  value       = [for k, v in azurerm_application_insights_smart_detection_rule.application_insights_smart_detection_rules : v.additional_email_recipients]
+  description = "Map of additional_email_recipients values across all application_insights_smart_detection_rules, keyed the same as var.application_insights_smart_detection_rules"
+  value       = { for k, v in azurerm_application_insights_smart_detection_rule.application_insights_smart_detection_rules : k => v.additional_email_recipients }
 }
 output "application_insights_smart_detection_rules_application_insights_id" {
-  description = "List of application_insights_id values across all application_insights_smart_detection_rules"
-  value       = [for k, v in azurerm_application_insights_smart_detection_rule.application_insights_smart_detection_rules : v.application_insights_id]
+  description = "Map of application_insights_id values across all application_insights_smart_detection_rules, keyed the same as var.application_insights_smart_detection_rules"
+  value       = { for k, v in azurerm_application_insights_smart_detection_rule.application_insights_smart_detection_rules : k => v.application_insights_id }
 }
 output "application_insights_smart_detection_rules_enabled" {
-  description = "List of enabled values across all application_insights_smart_detection_rules"
-  value       = [for k, v in azurerm_application_insights_smart_detection_rule.application_insights_smart_detection_rules : v.enabled]
+  description = "Map of enabled values across all application_insights_smart_detection_rules, keyed the same as var.application_insights_smart_detection_rules"
+  value       = { for k, v in azurerm_application_insights_smart_detection_rule.application_insights_smart_detection_rules : k => v.enabled }
 }
 output "application_insights_smart_detection_rules_name" {
-  description = "List of name values across all application_insights_smart_detection_rules"
-  value       = [for k, v in azurerm_application_insights_smart_detection_rule.application_insights_smart_detection_rules : v.name]
+  description = "Map of name values across all application_insights_smart_detection_rules, keyed the same as var.application_insights_smart_detection_rules"
+  value       = { for k, v in azurerm_application_insights_smart_detection_rule.application_insights_smart_detection_rules : k => v.name }
 }
 output "application_insights_smart_detection_rules_send_emails_to_subscription_owners" {
-  description = "List of send_emails_to_subscription_owners values across all application_insights_smart_detection_rules"
-  value       = [for k, v in azurerm_application_insights_smart_detection_rule.application_insights_smart_detection_rules : v.send_emails_to_subscription_owners]
+  description = "Map of send_emails_to_subscription_owners values across all application_insights_smart_detection_rules, keyed the same as var.application_insights_smart_detection_rules"
+  value       = { for k, v in azurerm_application_insights_smart_detection_rule.application_insights_smart_detection_rules : k => v.send_emails_to_subscription_owners }
 }
 
